caa249d678210032e401116c361570c89d135010
mark mwendwa <mwendwamark310@gmail.com>
2023-03-14 11:00:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  //write your code here
+  let message ;
+  if (feet<=400) {
+    message='This one is on me!'
+    return message;
+  }
+{
+  if (feet >2000 && feet<2500) {
+  message= 'I will gladly take your thirty bucks.'
+return message;
+}
+{if (feet>2500){
+message ='No can do.'
+return message;
+
+}}
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+  
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(City){
+  // Write your code here!
+if(City=== 'NYC'){
+  return"Ok, sounds good."}
+else {return "No go."}
+
+
+
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+case 'generous':
+return 'Thank you so much.'
+case 'not as generous':
+return'Thank you.'
+default:
+
+  return 'Bye.'
+
+  }
   // Write your code here!
 }
